24e7a46d25d7699b79273f56bb6a6cc4883f21a1
Leekangeun1 <graceke1@naver.com>
2023-12-05 14:35:12 +0900

    nothing changed

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('chatgpt/',include('chatgpt.urls')),
     path('signlanguagetochatgpt/',include('signlanguagetochatgpt.urls')),
     path('selfchatgpt/', include('selfchatgpt.urls')),
-    path('selfsignlanguagetochatgpt/', include('selfsignlanguagetochatgpt.urls'))
+    path('selfsignlanguagetochatgpt/', include('selfsignlanguagetochatgpt.urls')),
 ]
 
 from django.conf import settings
